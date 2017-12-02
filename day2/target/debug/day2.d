fe9b42d7c380e85c5d4aa37c3f593ea875b337f1
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2017-12-02 10:34:21 +0100

    cleanup, add gitignore

--- day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/Users/ta10/repositories/advent-of-code-2017/day2/target/debug/day2.dSYM: /Users/ta10/repositories/advent-of-code-2017/day2/src/main.rs
--- day2/target/rls/debug/libday2.d
+++
@@ -1,1 +0,0 @@
-/Users/ta10/repositories/advent-of-code-2017/day2/target/rls/debug/libday2.rmeta: /Users/ta10/repositories/advent-of-code-2017/day2/src/main.rs
